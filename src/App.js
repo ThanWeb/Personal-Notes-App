5f12ee9ebe18f290ff2370b9540dfdfe2016b323
Hans <classsickpeople@gmail.com>
2022-10-22 01:10:58 +0800

    Menambahkan fitur ubah tema

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import ListPage from './pages/ListPage';
 import ArchivePage from './pages/ArchivePage';
@@ -10,16 +10,35 @@ import Copyright from './components/Copyright';
 import { getUserLogged, putAccessToken } from './utils/network-data';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+// import { LanguageProvider } from './contexts/LanguageContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 
 function App() {
+    const navigate = useNavigate();
     const [authedUser, setAuthedUser] = React.useState(null);
     const [initialize, setInitialize] = React.useState(true);
+    // const [language, toggleLanguage] = React.useState();
+    const [theme, toggleTheme] = React.useState('');
 
     React.useEffect(() => {
         getLoginStatus();
         setInitialize(false);
+        getTheme();
     }, []);
+
+    const getTheme = () => {
+        let currentTheme = localStorage.getItem('theme') || 'light';
+        toggleTheme(currentTheme);
+        localStorage.setItem('theme', currentTheme);
+    }
+
+    const setTheme = () => {
+        let prevTheme = localStorage.getItem('theme');
+        let newTheme = prevTheme === 'light' ? 'dark' : 'light';
+        toggleTheme(newTheme);
+        localStorage.setItem('theme', newTheme)
+    }
 
     async function getLoginStatus() {
         const { data } = await getUserLogged();
@@ -35,6 +54,7 @@ function App() {
     const onLogout = () => {
         setAuthedUser(null);
         putAccessToken('');
+        navigate('/');
     }
 
     if (initialize) {
@@ -43,38 +63,44 @@ function App() {
 
     if (authedUser === null) {
         return (
-            <div className="personal-notes-app">
+            <ThemeProvider value={theme}>
+                <div className={theme === 'light' ? "personal-notes-app" : "personal-notes-app dark"}>
+                    <header className={theme === 'light' ? "personal-notes-header" : "personal-notes-header dark"}>
+                    </header>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<LoginPage loginSuccess={onLoginSuccess}/>} />
+                            <Route path="/register" element={<RegisterPage />} />
+                        </Routes>
+                    </main>
+                    <footer className='personal-notes-footer'>
+                        <Copyright />
+                    </footer>
+                </div>
+            </ThemeProvider>
+        );
+    }
+
+    return (
+        <ThemeProvider value={theme}>
+            <div className={theme === 'light' ? "personal-notes-app" : "personal-notes-app dark"}>
+                <header className={theme === 'light' ? "personal-notes-header" : "personal-notes-header dark"}>
+                    <Navigation logout={onLogout} setTheme={setTheme}/>
+                </header>
                 <main>
                     <Routes>
-                        <Route path="/" element={<LoginPage loginSuccess={onLoginSuccess}/>} />
-                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/" element={<ListPage />} />
+                        <Route path="/archive" element={<ArchivePage />} />
+                        <Route path="/add" element={<AddPage />} />
+                        <Route path="/detail/:id" element={<DetailPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
                     </Routes>
                 </main>
                 <footer className='personal-notes-footer'>
                     <Copyright />
                 </footer>
             </div>
-        );
-    }
-
-    return (
-        <div className="personal-notes-app">
-            <header className='personal-notes-header'>
-                <Navigation logout={onLogout} />
-            </header>
-            <main>
-                <Routes>
-                    <Route path="/" element={<ListPage />} />
-                    <Route path="/archive" element={<ArchivePage />} />
-                    <Route path="/add" element={<AddPage />} />
-                    <Route path="/detail/:id" element={<DetailPage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-            </main>
-            <footer className='personal-notes-footer'>
-                <Copyright />
-            </footer>
-        </div>
+        </ThemeProvider>
     );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeConsumer } from '../contexts/ThemeContext';
  
-function Navigation({ logout }) {
+function Navigation({ logout, setTheme }) {
     return (
-        <nav className="navigation">
-            <div>
-                <Link to="/" className='notes-button home-page-button'>
-                    <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/home_icon.png' alt='home'/>
-                    <span>Home</span>
-                </Link>
-            </div>
-            {/* <div>
-                <h2>Not just a Note</h2>
-            </div> */}
-            <Link to="/add" className='notes-button add-page-button'>
-                <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/add_circle_icon.png' alt='add'/>
-            </Link>
-            <div>
-                <Link to="/archive" className='notes-button archive-page-button'>
-                    <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/bookmark_icon.png' alt='archive'/>
-                    <span>Archived</span>
-                </Link>
-                <button className='notes-button log-out-button' onClick={logout}>
-                    <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/logout-icon.png' alt='log-out'/>
-                    <span>Log out</span>
-                </button>
-            </div>
-        </nav>
+        <ThemeConsumer>
+            {
+                (theme) => {
+                    return (
+                        <nav className="navigation">
+                            <div>
+                                <Link to="/" className='notes-button home-page-button'>
+                                    <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/home_icon.png' alt='home'/>
+                                    <span>Home</span>
+                                </Link>
+                            </div>
+                            <img onClick={setTheme} className='notes-button theme-button' src={theme === 'light' ? 'http://antekteknologi.my.id/wp-content/uploads/2022/10/light_mode_icon.png' : 'http://antekteknologi.my.id/wp-content/uploads/2022/10/dark_mode_icon.png'} alt='change-theme'/>
+                            <Link to="/add" className='notes-button add-page-button'>
+                                <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/add_circle_icon.png' alt='add'/>
+                            </Link>
+                            <div>
+                                <Link to="/archive" className='notes-button archive-page-button'>
+                                    <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/bookmark_icon.png' alt='archive'/>
+                                    <span>Archived</span>
+                                </Link>
+                                <button className='notes-button log-out-button' onClick={logout}>
+                                    <img src='http://antekteknologi.my.id/wp-content/uploads/2022/10/logout-icon.png' alt='log-out'/>
+                                    <span>Log out</span>
+                                </button>
+                            </div>
+                    </nav>
+                    );
+                }
+            }
+        </ThemeConsumer>
     );
 }
  
